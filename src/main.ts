b2ea832bf0a005ddf14b5a60a1e373160eee86f9
namitchp <namitchp@gmail.com>
2025-02-03 17:22:24 +0530

    we have install sharp ,s3 and multer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { exec } from 'child_process';
 
 // console.log('PORT:', process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  exec('ls -l', (err, stdout, stderr) => {
+
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (stderr) {
+      console.error(stderr);
+      return;
+    }
+    console.log(stdout);
+  });
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
