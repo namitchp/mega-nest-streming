63d1d2bfd2be4693aee5d6da567879bcb96f9ac9
namitchp <namitchp@gmail.com>
2025-02-04 12:12:29 +0530

    today working on file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './apis/users.controller';
 // import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),],
   // imports: [UsersModule],
   controllers: [AppController, UserController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { exec } from 'child_process';
 
-// console.log('PORT:', process.env.PORT);
+console.log('PORT:', process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  exec('ls -l', (err, stdout, stderr) => {
+  // exec('ls -l', (err, stdout, stderr) => {
 
-    if (err) {
-      console.error(err);
-      return;
-    }
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
 
-    if (stderr) {
-      console.error(stderr);
-      return;
-    }
-    console.log(stdout);
-  });
-  await app.listen(process.env.PORT ?? 8000);
+  //   if (stderr) {
+  //     console.error(stderr);
+  //     return;
+  //   }
+  //   console.log(stdout);
+  // });
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
