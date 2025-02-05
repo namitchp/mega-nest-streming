14978d757488c42c27bc565d74c1018e9e499319
namitchp <namitchp@gmail.com>
2025-02-05 23:51:09 +0530

    image

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { exec } from 'child_process';
+import sharp from 'sharp';
 
 console.log('PORT:', process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  sharp('src/assets/1.jpg');
   // exec('ls -l', (err, stdout, stderr) => {
 
   //   if (err) {
