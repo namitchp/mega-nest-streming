e7a47278fc3fe74c41121fe30f0cc0dddedbbac0
namitchp <namitchp@gmail.com>
2025-02-09 23:06:07 +0530

    workind on vide upload using multer

--- src/apis/video.controller.ts
+++ src/apis/video.controller.ts
@@ -1,12 +1,14 @@
 import {
   Controller,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { VideoStremingService } from '../core/services/video.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { imageUpload } from 'src/helpers/fileUpload';
+import { Response } from 'express';
 // import { StremingService } from 'src/core/services/video.service';
 @Controller('/video')
 export class VideoController {
@@ -14,9 +16,11 @@ export class VideoController {
   // console.log(common.imageUpload('uploads', 10));
 
   @Post('/upload')
-  @UseInterceptors(FileInterceptor('file', imageUpload('uploads/video', 10)))
+  @UseInterceptors(
+    FileInterceptor('file', imageUpload('uploads/video/main', 1000)),
+  )
   // async getVideo(@Body() body: any): Promise<void> {
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return this.videoStreamingService.uploadVideo(file);
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Res() res: Response) {
+    return this.videoStreamingService.uploadVideo(file, res);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { json, urlencoded, static as serveStatic } from 'express';
 import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: false }));
   app.use(serveStatic(join(__dirname, '../uploads')));
