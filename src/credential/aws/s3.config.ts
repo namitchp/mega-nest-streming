f756dbe0a1864f3cf51b0a404679f6c7eaa260bd
namitchp <namitchp@gmail.com>
2025-02-12 11:03:09 +0530

    working on s3 bucket

--- src/credential/aws/s3.config.ts
+++ src/credential/aws/s3.config.ts
@@ -7,10 +7,10 @@ import { S3Client } from '@aws-sdk/client-s3';
 //         secretAccessKey: process.env.AWS_SECRETACCESSKEY, // Secret access key from environment variables
 //     },
 // });
-export const s3ClientImage = new S3Client({
-    region: 'ap-south-1',
-    credentials: {
-        accessKeyId: 'AKIAYHVAFRZZXYBFPDMS', // Access key ID from environment variables
-        secretAccessKey: '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', // Secret access key from environment variables
-    },
-});
+// export const s3ClientImage = new S3Client({
+//     region: 'ap-south-1',
+//     credentials: {
+//         accessKeyId: 'AKIAYHVAFRZZXYBFPDMS', // Access key ID from environment variables
+//         secretAccessKey: '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', // Secret access key from environment variables
+//     },
+// });
