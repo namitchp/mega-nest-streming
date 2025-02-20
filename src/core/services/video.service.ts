5148e8b89cca6836fad96d8181972db52f568067
namitchp <namitchp@gmail.com>
2025-02-20 23:17:31 +0530

    working on converting vide quality like 240,380,440

--- src/core/services/video.service.ts
+++ src/core/services/video.service.ts
@@ -1,6 +1,6 @@
-import { HttpCode, HttpStatus, Injectable } from "@nestjs/common";
-import { exec } from "child_process";
-import { CommonFunction } from "src/helpers/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { exec } from 'child_process';
+import { CommonFunction } from 'src/helpers/common';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 import { json } from 'express';
@@ -15,9 +15,13 @@ export class VideoStremingService extends CommonFunction {
     if (!fs.existsSync(outputPath)) {
       fs.mkdirSync(outputPath);
     }
-    // ffmpeg streaming
-    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/stream%03d" -start_number 0 ${hlsPath}`;
-    // const ffmpegCommand = `ffmpeg -i ${videoPath} -c:v libx264 -preset veryfast -b:v 1000k -c:a aac -b:a 128k -hls_time 8 -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath} `;
+    // ffmpeg streaming for multiple qualities
+    const ffmpegCommand = `ffmpeg -i ${videoPath} \
+      -vf "scale=w=426:h=240" -c:v libx264 -b:v 250k -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/240p_%03d.ch" -start_number 1 ${outputPath}/240p.m3u8 \
+      -vf "scale=w=640:h=360" -c:v libx264 -b:v 400k -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/360p_%03d.ch" -start_number 1 ${outputPath}/360p.m3u8 \
+      -vf "scale=w=854:h=480" -c:v libx264 -b:v 500k -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/480p_%03d.ch" -start_number 1 ${outputPath}/480p.m3u8 \
+      -vf "scale=w=1280:h=720" -c:v libx264 -b:v 700k -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/720p_%03d.ch" -start_number 1 ${outputPath}/720p.m3u8 \
+      -vf "scale=w=1920:h=1080" -c:v libx264 -b:v 800k -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/1080p_%03d.ch" -start_number 1 ${outputPath}/1080p.m3u8`;
     exec(ffmpegCommand, (error, stdout, stderr) => {
       if (error) {
         console.log(`exec error: ${error}`);
